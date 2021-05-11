500e23cb146a49d8f69b555460963dc3d0ec26c2
Bushra <77388684+bushra1997@users.noreply.github.com>
2021-05-11 14:11:27 +0300

    Done with Practice 5 && 6

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const axios = require("axios");
 const express = require("express");
+const { clear } = require("console");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
@@ -49,21 +50,55 @@ const getPostAsync = async (id) => {
 // getPostAsync(15)
 
 const appendToFile = (data) => {
-    fs.appendFile("data.txt", `\n ${data}`, (err)=>{
-        if (err) throw err;
-        console.log("data appended")
-    })
+  fs.appendFile("data.txt", `\n ${data}`, (err) => {
+    if (err) throw err;
+    console.log("data appended");
+  });
 };
 // appendToFile("hello")
 
-
 const copyFile = (fileName) => {
-    fs.copyFile(fileName, `copy_of_${fileName}`, (err)=>{
-        if (err) throw err;
-        console.log("Done")
-    })
+  fs.copyFile(fileName, `copy_of_${fileName}`, (err) => {
+    if (err) throw err;
+    console.log("Done");
+  });
 };
 // copyFile("data.txt")
+const post = JSON.stringify({
+  title: "JavaScript Basics",
+  body: "This post contains information about javaScript ",
+  // the id of the user who is going to create the post
+  userId: 1,
+});
+// console.log(post);
+const createPost = (user) => {
+  axios({
+    method: "post",
+    url: "https://jsonplaceholder.typicode.com/posts",
+    data: post,
+  });
+};
+// createPost()
+// clear()
+const getUsers = async () => {
+    try {
+        const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+        let users = JSON.stringify(response.data);
+        const saveUsers = (users) => {
+            fs.writeFile("users.txt", users, (err)=>{
+                if(err) throw err
+            })
+        };
+        saveUsers(users)
+      } catch (err) {
+        throw err;
+      }
+}
+// getUsers();
+
+
 app.listen(port, () => {
   // console.log(`app listening at http://localhost:${port}`);
 });
